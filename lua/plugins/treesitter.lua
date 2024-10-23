981e4158577df05ccf5fc802378bff8242f24434
P2Tree <dicksonliuming@gmail.com>
2024-10-23 10:40:41 +0800

    Update treesitter.lua
    Treesitter 增加 endwise 插件，以及一些其他配置

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,16 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "RRethy/nvim-treesitter-endwise",
+  },
   opts = {
     ensure_installed = {
       "lua",
       "vim",
       "cpp",
       "c",
+      "rust",
       "python",
       "llvm",
       "tablegen",
@@ -19,5 +23,16 @@ return {
       "markdown",
       -- add more arguments for adding more treesitter parsers
     },
+    indent = { enable = false },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        node_decremental = "<BS>",
+        scope_incremental = "<TAB>",
+      },
+    },
+  endwise = { enable = true },
   },
 }

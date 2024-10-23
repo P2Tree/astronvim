358132f45635a50cbb155422a308abf3a0886361
P2Tree <dicksonliuming@gmail.com>
2024-10-23 10:38:01 +0800

    Update codeium.lua
    Codeium 的补全快捷键改为 ctrl-f

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,6 +2,7 @@ return {
   "Exafunction/codeium.vim",
   event = "User AstroFile",
   config = function()
+    vim.keymap.set("i", "<C-f>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
     vim.keymap.set("i", "<Right>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
     vim.keymap.set("i", "<Down>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
     vim.keymap.set("i", "<Up>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
